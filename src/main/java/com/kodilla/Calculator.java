29ec2c10923a3609db34e3b5651b6bed2f26ffd8
Krzysztof Sobkowiak <krzysztof_91@hotmail.com>
2020-12-08 17:24:55 +0100

    Kalkulator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -2,4 +2,26 @@ package com.kodilla;
 
 public class Calculator {
 
+    public void calculaotrAddMethod() {
+        int add1 = 5;
+        int add2 = 10;
+
+        System.out.println("Add: " + (add1 + add2));
+    }
+
+    public void calculatorSubstractMethod() {
+        double substract1 = 1.8;
+        double substract2 = 0.8;
+
+        System.out.println("Syubstract: " + (substract1 - substract2));
+    }
+
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.calculaotrAddMethod();
+        calculator.calculatorSubstractMethod();
+    }
 }
+
+
